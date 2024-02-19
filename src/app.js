b4b93c24386bfe4a4f3a5c918ff198cb4b9dc8c2
wswani <wasit@superfitout.com>
2024-02-20 03:56:22 +0530

    updated api route for FE proxy

--- src/app.js
+++ src/app.js
@@ -2,13 +2,16 @@ require('dotenv').config();
 const express = require('express');
 
 const app = express();
-const PORT = process.env.PORT || 4200;
+const PORT = process.env.PORT || 5200;
+
+// const cors = require('cors');
+// app.use(cors());
 
 app.get('/', (req, res) => {
 	res.json({ msg: 'Hello World from Nginx/Apache HTTP Server' });
 });
 
-app.get('/about', (req, res) => {
+app.get('/api/about', (req, res) => {
 	res.json({
 		about:
 			"There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or  words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.",
